f5eb017ef08fce801402b8bbd576e78492f46be7
kamya <phove21@student.aau.dk>
2025-04-04 10:34:04 +0200

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("io.github.andrefigas.rustjni") version "0.0.23"
+}
+
+android {
+    namespace = "com.example.rust3"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.rust3"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+rustJni{
+    rustPath = "./app/src/main/rust"
+    ndkVersion = "29.0.13113456"
+    architectures {
+        armv7_linux_androideabi("armv7a-linux-androideabi21-clang")
+        aarch64_linux_android("aarch64-linux-android21-clang")
+        i686_linux_android("i686-linux-android21-clang")
+        x86_64_linux_android("x86_64-linux-android21-clang")
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
